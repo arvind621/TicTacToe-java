93f1cc22e39a98de0601b90c1f871bf9e599a45e
arvind <arvindvemula519@gmail.com>
2024-08-20 13:14:29 +0530

    Add files via upload

---
+++ TicTacToe.java
@@ -0,0 +1,109 @@
+import java.util.Scanner;
+
+public class TicTacToe {
+
+    private static final char[][] board = new char[3][3];  
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        initializeBoard();
+        displayBoard();
+
+        while (true) {
+            playerInput(scanner); 
+            displayBoard();
+            if (checkWinner()) {
+                System.out.println("Player " + currentPlayer + " wins!");
+                break;
+            }
+            if (isBoardFull()) {
+                System.out.println("It's a tie!");
+                break;
+            }
+            switchPlayer();
+        }
+
+        if (restartGame(scanner)) { 
+            main(new String[0]);
+        } else {
+            System.out.println("Thanks for playing!");
+        }
+
+        scanner.close(); 
+    }
+
+    public static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
+            }
+        }
+        System.out.println("Tic Tac Toe Game Initialized! Player X goes first.");
+    }
+
+    public static void displayBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void playerInput(Scanner scanner) {
+        int row, col;
+
+        while (true) {
+            System.out.println("Player " + currentPlayer + ", enter your move (row and column): ");
+            row = scanner.nextInt() - 1;
+            col = scanner.nextInt() - 1;
+
+            if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == '-') {
+                board[row][col] = currentPlayer;
+                break;
+            } else {
+                System.out.println("This move is not valid");
+            }
+        }
+    }
+
+    public static boolean checkWinner() {
+        
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) ||
+                (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer)) {
+                return true;
+            }
+        }
+
+        
+        if ((board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer) ||
+            (board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void switchPlayer() {
+        currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+    }
+
+    public static boolean restartGame(Scanner scanner) {
+        System.out.println("Do you want to play again? (yes/no): ");
+        String response = scanner.nextLine().toLowerCase();
+        return response.equals("yes");
+    }
+}
